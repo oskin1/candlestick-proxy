7c4b9b667137a605fce2900075d97b0a7ed04da0
oskin1 <ilya.arcadich@gmail.com>
2019-10-20 17:55:06 +0300

    Minor improvements.

--- src/main/scala/com/github/oskin1/proxy/App.scala
+++ src/main/scala/com/github/oskin1/proxy/App.scala
@@ -11,25 +11,26 @@ import configReaders._
 object App extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] =
-    loadSettings
-      .flatMap { settings =>
-        Slf4jLogger.create[IO].flatMap { implicit logger =>
-          Blocker[IO].use { blocker =>
-            SocketGroup[IO](blocker).use { socketGroup =>
-              TradesPersistence
-                .empty(settings.keepLastEpochs)
-                .flatMap { ref =>
-                  TradesListener
-                    .run(ref, socketGroup, settings)
-                    .concurrently(Server.run(ref, socketGroup, settings))
-                    .compile
-                    .drain
-                }
-            }
+    program.as(ExitCode.Success)
+
+  private def program: IO[Unit] =
+    loadSettings.flatMap { settings =>
+      Slf4jLogger.create[IO].flatMap { implicit logger =>
+        Blocker[IO].use { blocker =>
+          SocketGroup[IO](blocker).use { socketGroup =>
+            TradesPersistence
+              .empty(settings.keepLastEpochs)
+              .flatMap { ref =>
+                TradesListener
+                  .run(ref, socketGroup, settings)
+                  .concurrently(Server.run(ref, socketGroup, settings))
+                  .compile
+                  .drain
+              }
           }
         }
       }
-      .as(ExitCode.Success)
+    }
 
   private def loadSettings: IO[Settings] =
     IO.delay(ConfigSource.default.load[Settings]).flatMap {
